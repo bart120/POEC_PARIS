7aaf86ed4a69ba63f40b473fe3e4bb6be3418be8
Vincent <vleclerc@inow.fr>
2018-09-24 15:35:53 +0200

    Ajout du projet vide ASP MVC 5. Ajout d'un controleur Home. Ajout des vues Index et About pour Home. Ajout du model Info.

---
+++ AspMVC/Archery/Archery/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Archery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var modelInfo = new Info
+            {
+                DevName = "Vincent",
+                ContactMail = "vincent@vincent.com",
+                CreatedDate = DateTime.Now
+            };
+            return View(modelInfo);
+        }
+
+    }
+}
---
+++ AspMVC/Archery/Archery/Models/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    public class Info
+    {
+        public string DevName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string ContactMail { get; set; }
+
+    }
+}

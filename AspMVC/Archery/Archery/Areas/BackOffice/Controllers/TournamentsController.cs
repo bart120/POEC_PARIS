cae7eba573079473fdc3ddf7750f7ce9369d3de6
Vincent <vleclerc@inow.fr>
2018-10-02 11:40:34 +0200

    ajout description controllers et views Tournament

--- AspMVC/Archery/Archery/Areas/BackOffice/Controllers/TournamentsController.cs
+++ AspMVC/Archery/Archery/Areas/BackOffice/Controllers/TournamentsController.cs
@@ -47,7 +47,7 @@ namespace Archery.Areas.BackOffice.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Name,Location,StartDate,EndDate,ArcherCount,Price")] Tournament tournament)
+        public ActionResult Create([Bind(Include = "Name,Location,StartDate,EndDate,ArcherCount,Price,Description")] Tournament tournament)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace Archery.Areas.BackOffice.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Name,Location,StartDate,EndDate,ArcherCount,Price")] Tournament tournament)
+        public ActionResult Edit([Bind(Include = "ID,Name,Location,StartDate,EndDate,ArcherCount,Price,Description")] Tournament tournament)
         {
             if (ModelState.IsValid)
             {

ddc3a990f339b1cf7fa9b1bd13fa53934faa3100
Vincent <vleclerc@inow.fr>
2018-10-02 10:42:56 +0200

    scafold pour tournaments

---
+++ AspMVC/Archery/Archery/Areas/BackOffice/Controllers/TournamentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Archery.Data;
+using Archery.Models;
+
+namespace Archery.Areas.BackOffice.Controllers
+{
+    public class TournamentsController : Controller
+    {
+        private ArcheryDbContext db = new ArcheryDbContext();
+
+        // GET: BackOffice/Tournaments
+        public ActionResult Index()
+        {
+            return View(db.Tournaments.ToList());
+        }
+
+        // GET: BackOffice/Tournaments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tournament = db.Tournaments.Find(id);
+            if (tournament == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tournament);
+        }
+
+        // GET: BackOffice/Tournaments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackOffice/Tournaments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Location,StartDate,EndDate,ArcherCount,Price")] Tournament tournament)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tournaments.Add(tournament);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tournament);
+        }
+
+        // GET: BackOffice/Tournaments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tournament = db.Tournaments.Find(id);
+            if (tournament == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tournament);
+        }
+
+        // POST: BackOffice/Tournaments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Location,StartDate,EndDate,ArcherCount,Price")] Tournament tournament)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tournament).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tournament);
+        }
+
+        // GET: BackOffice/Tournaments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tournament tournament = db.Tournaments.Find(id);
+            if (tournament == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tournament);
+        }
+
+        // POST: BackOffice/Tournaments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tournament tournament = db.Tournaments.Find(id);
+            db.Tournaments.Remove(tournament);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

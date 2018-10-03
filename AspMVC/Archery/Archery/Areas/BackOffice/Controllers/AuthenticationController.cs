bb52439c962851351e03e80a0c72f95ffa61eb34
Vincent <vleclerc@inow.fr>
2018-10-03 15:31:07 +0200

    ajout du controller viewmodel et view pour l'authentification du BO.

---
+++ AspMVC/Archery/Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using Archery.Areas.BackOffice.Models;
+using Archery.Controllers;
+using Archery.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Archery.Areas.BackOffice.Controllers
+{
+    public class AuthenticationController : BaseController
+    {
+        // GET: BackOffice/Authentication
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AuthenticationLoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var hash = model.Password.HashMD5();
+                var admin = db.Administrators.SingleOrDefault(
+                    x => x.Mail == model.Mail && x.Password == hash);
+
+                if(admin == null)
+                {
+                    ModelState.AddModelError("Mail", "Login / mot de passe invalide");
+                    return View();
+                }
+                else
+                {
+
+                    return RedirectToAction("Index", "Dashboard", new { area = "backoffice" });
+                }
+
+            }
+            return View();
+        }
+
+    }
+}
---
+++ AspMVC/Archery/Archery/Areas/BackOffice/Models/AuthenticationViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Areas.BackOffice.Models
+{
+    public class AuthenticationLoginViewModel
+    {
+        [Display(Name = "Login")]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Mot de passe")]
+        [Required(ErrorMessage = "Le champ {0} est obligatoire.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

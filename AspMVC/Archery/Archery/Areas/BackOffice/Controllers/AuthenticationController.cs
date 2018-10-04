ce3893b04676c655fd82380d1ce21171786dafc0
Vincent <vleclerc@inow.fr>
2018-10-04 10:09:25 +0200

    ajout du logout

--- AspMVC/Archery/Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
+++ AspMVC/Archery/Archery/Areas/BackOffice/Controllers/AuthenticationController.cs
@@ -42,5 +42,11 @@ namespace Archery.Areas.BackOffice.Controllers
             return View();
         }
 
+        public ActionResult Logout()
+        {
+            Session.Remove("ADMINISTRATOR");
+            return RedirectToAction("index", "home", new { area = "" });
+        }
+
     }
 }
